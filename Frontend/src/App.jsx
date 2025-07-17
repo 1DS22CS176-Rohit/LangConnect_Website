9d4274fa634e23e6dbd527ca72253de3c920dc1e
Rohit K S <rohit@gmail.com>
2025-07-17 20:28:09 +0530

    Chat Page and Call Page complete

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -61,8 +61,30 @@ const App = () => {
           path="/onboarding"
           element={isAuthenticated ? !isOnboarded ? <OnboardingPage /> : <Navigate to="/" /> : <Navigate to="/login" />}
         />
-        <Route path="/call" element={isAuthenticated ? <CallPage /> : <Navigate to="/login" />} />
-        <Route path="/chat" element={isAuthenticated ? <ChatPage /> : <Navigate to="/login" />} />
+        <Route
+          path="/call/:id"
+          element={
+            isAuthenticated && isOnboarded ? (
+              <Layout showSidebar={false}>
+                <CallPage />
+              </Layout>
+            ) : (
+              <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+            )
+          }
+        />
+        <Route
+          path="/chat/:id"
+          element={
+            isAuthenticated && isOnboarded ? (
+              <Layout showSidebar={false}>
+                <ChatPage />
+              </Layout>
+            ) : (
+              <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+            )
+          }
+        />
       </Routes>
       <Toaster />
     </div>
