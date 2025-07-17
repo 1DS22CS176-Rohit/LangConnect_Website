687c1a9608c91f0aa75d8214f50a68c5558b64bf
Rohit K S <rohit@gmail.com>
2025-07-17 22:19:24 +0530

    Deployment changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -40,3 +40,5 @@ app.listen(PORT, () => {
   console.log("Server is running on ", PORT, "......");
   connectDB();
 });
+
+//
--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -64,3 +64,5 @@ export async function getStreamToken() {
   const res = await axiosInstance.get("/chat/token");
   return res.data;
 }
+
+//
